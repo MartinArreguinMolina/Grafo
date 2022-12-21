4ca074f0a03e661cfa76a2c0a03e7e9f10d93fb8
Martin Arreguin <arrec7281@gmail.com>
2022-12-20 21:06:42 -0600

    Se cambio el nombre del proyecto

--- src/PaqueteUno/Nodo.java
+++ src/PaqueteUno/Nodo.java
@@ -32,6 +32,6 @@ public class Nodo {
     }
     
     public String toString(){
-        return "Ciudad \n" + getCiudad() + ", Arista \n" + getLista();
+        return "Ciudad \n" + getCiudad() + ", \nArista " + getLista();
     }
 }
